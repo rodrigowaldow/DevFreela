298ba5f18b2eb7e8bdb9bf3c686d52a2ea646168
Rodrigo Waldow <rodrigo.waldow@conectt.com.br>
2021-06-29 22:33:17 -0300

    add methods to service application

--- DevFreela.Application/InputModels/CreateCommentInputModel.cs
+++ DevFreela.Application/InputModels/CreateCommentInputModel.cs
@@ -2,5 +2,8 @@
 {
     public class CreateCommentInputModel
     {
+        public string Content { get; private set; }
+        public int IdProject { get; private set; }
+        public int IdUser { get; private set; }
     }
 }
